f339b664cd0fd4be54d3aed1ae87576464e8cddd
Daiki48 <daiki48.engineer@gmail.com>
2022-09-16 16:38:58 +0900

    [update] post adding

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,10 @@ use diesel::{
 use actix_web::web::Data;
 use actix_web::{
     get,
+    post,
     web,
     App,
-    // HttpResponse,
+    HttpResponse,
     HttpServer,
     Responder,
     Result,
@@ -33,6 +34,20 @@ async fn get(db: web::Data<db::Pool>, path: web::Path<i32>) -> Result<impl Respo
     .expect("error");
 
     Ok(web::Json(user))
+}
+
+#[post("/users")]
+async fn post (db: web::Data<db::Pool>, item: web::Json<model::User>) -> Result<impl Responder> {
+    let mut conn = db.get().unwrap();
+    let new_user = model::User {
+        email: item.email.to_string(),
+    };
+    diesel::insert_into(schema::users::dsl::users)
+    .values(&new_user)
+    .execute(&mut conn)
+    .expect("Error saving new post");
+
+    Ok(HttpResponse::Created().body("get ok"))
 }
 
 #[actix_web::main]
